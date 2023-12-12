246b7ad75fbcd756e3ece521aeb63510e36252d5
mahirlabibdihan <mahirlabibdihan@gmail.com>
2023-12-12 18:13:11 +0600

    .

--- CSE410 - Offline01/Workspace/Vec3.cpp
+++ CSE410 - Offline01/Workspace/Vec3.cpp
@@ -4,17 +4,11 @@ Vec3::Vec3() : x(0), y(0), z(0) {}
 Vec3::Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 Vec3::Vec3(const Vec3 &p) : x(p.x), y(p.y), z(p.z) {}
 Vec3 Vec3::operator+(Vec3 b) { return Vec3(x + b.x, y + b.y, z + b.z); }
-Vec3 Vec3::operator+=(Vec3 b) { return Vec3(x + b.x, y + b.y, z + b.z); }
 Vec3 Vec3::operator-(Vec3 b) { return Vec3(x - b.x, y - b.y, z - b.z); }
-Vec3 Vec3::operator-=(Vec3 b) { return Vec3(x - b.x, y - b.y, z - b.z); }
 Vec3 Vec3::operator*(double b) { return Vec3(x * b, y * b, z * b); }
-Vec3 Vec3::operator*=(double b) { return Vec3(x * b, y * b, z * b); }
 Vec3 Vec3::operator/(double b) { return Vec3(x / b, y / b, z / b); }
-Vec3 Vec3::operator/=(double b) { return Vec3(x / b, y / b, z / b); }
 Vec3 Vec3::operator/(Vec3 b) { return Vec3(x / b.x, y / b.y, z / b.y); }
-Vec3 Vec3::operator/=(Vec3 b) { return Vec3(x / b.x, y / b.y, z / b.y); }
 Vec3 Vec3::operator*(Vec3 b) { return Vec3(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
-Vec3 Vec3::operator*=(Vec3 b) { return Vec3(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
 
 double dot(Vec3 a, Vec3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
 double dot2D(Vec3 a, Vec3 b) { return a.x * b.x + a.y * b.y; }
